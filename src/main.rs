e09e6bd1ddcb85c244804cb1c4e4dea225373805
Griffin Sectio <grifin.dwi13@gmail.com>
2024-06-04 11:56:42 +0700

    Add tests and using struct for each temperature units

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,140 @@
 use dialoguer::{Confirm, Input, Select};
 use console::Term;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn f_to_c() {
+        let fahrenheit = Fahrenheit {
+            value: 50.5,
+            symbol: "F".to_string(),
+        };
+        let conversion_result = Celsius::from(fahrenheit);
+        assert_eq!("10.28C", format!("{:.2}{}", conversion_result.value, conversion_result.symbol))
+    }
+    
+    #[test]
+    fn k_to_c() {
+        let kelvin = Kelvin {
+            value: 325.5,
+            symbol: "K".to_string(),
+        };
+        let conversion_result = Celsius::from(kelvin);
+        assert_eq!("52.35C", format!("{:.2}{}", conversion_result.value, conversion_result.symbol))
+    }
+
+    #[test]
+    fn c_to_f() {
+        let celsius = Celsius {
+            value: 87.6,
+            symbol: "C".to_string(),
+        };
+        let conversion_result = Fahrenheit::from(celsius);
+        assert_eq!("189.68F", format!("{:.2}{}", conversion_result.value, conversion_result.symbol))
+    }
+
+    #[test]
+    fn k_to_f() {
+        let kelvin = Kelvin {
+            value: 325.5,
+            symbol: "K".to_string(),
+        };
+        let conversion_result = Fahrenheit::from(kelvin);
+        assert_eq!("126.23F", format!("{:.2}{}", conversion_result.value, conversion_result.symbol))
+    }
+
+    #[test]
+    fn c_to_k() {
+        let celsius = Celsius {
+            value: 28.15,
+            symbol: "C".to_string()
+        }; 
+        let conversion_result = Kelvin::from(celsius);
+        assert_eq!("301.30K", format!("{:.2}{}", conversion_result.value, conversion_result.symbol))
+    }
+
+    #[test]
+    fn f_to_k() {
+        let fahrenheit = Fahrenheit {
+            value: 180.23,
+            symbol: "F".to_string()
+        }; 
+        let conversion_result = Kelvin::from(fahrenheit);
+        assert_eq!("355.50K", format!("{:.2}{}", conversion_result.value, conversion_result.symbol))
+    }
+}
+
+struct Celsius {
+    value: f64,
+    symbol: String,
+}
+
+impl From<Fahrenheit> for Celsius {
+    fn from(unit: Fahrenheit) -> Self {
+        Celsius {
+            value: ((unit.value as f64 - 32.0) * 5.0) / 9.0,
+            symbol: "C".to_string(),
+        }
+    }
+}
+
+impl From<Kelvin> for Celsius {
+    fn from(unit: Kelvin) -> Self {
+        Celsius {
+            value: unit.value as f64 - 273.15,
+            symbol: "C".to_string(),
+        }
+    }
+}
+
+struct Fahrenheit {
+    value: f64,
+    symbol: String,
+}
+
+impl From<Celsius> for Fahrenheit {
+    fn from(unit: Celsius) -> Self {
+        Fahrenheit {
+            value: ((unit.value as f64 * 9.0) / 5.0) + 32.0,
+            symbol: "F".to_string(),
+        }
+    }
+}
+
+impl From<Kelvin> for Fahrenheit {
+    fn from(unit: Kelvin) -> Self {
+        Fahrenheit {
+            value: (((unit.value as f64 - 273.15) * 9.0) / 5.0) + 32.0,
+            symbol: "F".to_string(),
+        }
+    }
+}
+
+struct Kelvin {
+    value: f64,
+    symbol: String,
+}
+
+impl From<Celsius> for Kelvin {
+    fn from(unit: Celsius) -> Self {
+        Kelvin {
+            value: unit.value as f64 + 273.15,
+            symbol: "K".to_string()
+        }
+    }
+}
+
+impl From<Fahrenheit> for Kelvin {
+    fn from(unit: Fahrenheit) -> Self {
+        Kelvin {
+            value: (((unit.value as f64 - 32.0) * 5.0) / 9.0) + 273.15,
+            symbol: "K".to_string()
+        }
+    }
+}
+
 fn temperature(term: &Term) {
     let units = vec!["Celcius", "Fahrenheit", "Kelvin", "back"];
     let mut first_unit: String;
